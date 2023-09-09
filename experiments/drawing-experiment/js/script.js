a399ea7dcdf0f9abfbaa9c16bb8db45a7415d0ae
nancyhe1 <nancyhe098@gmail.com>
2023-09-09 18:42:38 -0400

    drawing experiment: added color to my cone

--- experiments/drawing-experiment/js/script.js
+++ experiments/drawing-experiment/js/script.js
@@ -22,12 +22,23 @@ function preload() {
 function setup() {
 createCanvas(500,500);
 
-background(200, 200, 400);
+background(150, 100, 255);
+
 ellipseMode(CORNER);
+noStroke();
+fill(245, 186, 122, 100);
 ellipse(250, 250, 100, 100);
+
+fill(235, 186, 132, 100);
 ellipse(250,250,80,80);
+
+fill(225, 186, 142, 100);
 ellipse(250,250,60,60);
+
+fill(215, 186, 152, 100);
 ellipse(250,250,40,40);
+
+fill(205, 186, 152, 100);
 ellipse(250,250,20,20);
 }
 
