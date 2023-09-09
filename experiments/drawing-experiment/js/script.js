487dc362fe2e7dffbe85354d0626334c66cbf8a7
nancyhe1 <nancyhe098@gmail.com>
2023-09-09 18:27:56 -0400

    drawing experiment: draw a cone

--- experiments/drawing-experiment/js/script.js
+++ experiments/drawing-experiment/js/script.js
@@ -20,7 +20,15 @@ function preload() {
  * Description of setup
 */
 function setup() {
+createCanvas(500,500);
 
+background(200, 200, 400);
+ellipseMode(CORNER);
+ellipse(250, 250, 100, 100);
+ellipse(250,250,80,80);
+ellipse(250,250,60,60);
+ellipse(250,250,40,40);
+ellipse(250,250,20,20);
 }
 
 
