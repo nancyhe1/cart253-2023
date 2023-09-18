80dbf5ef833fb615aa0b9de83f9fd1a59d561f3e
nancyhe1 <nancyhe098@gmail.com>
2023-09-18 18:53:45 -0400

    E1: added a cat shaped
    now i have to figure how to move this shape

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -27,7 +27,14 @@ function preload() {
 */
 function setup() {
 createCanvas(700,700);
+background(bg.r,bg.g,bg.b);
 noStroke();
+angleMode(DEGREES);
+//making a cat,the bongo cat!!
+fill(255);
+arc(350,350,200,200,180,0);
+triangle(255,320,280,220,350,320);
+triangle(350,320,410,220,445,320);
 }
 
 
@@ -39,13 +46,13 @@ function draw() {
 background(bg.r,bg.g,bg.b);
 //circle
 fill(mouseX, mouseY, mouseX);
-ellipse(mouseX, 250, mouseY);
+ellipse(mouseX, 350, mouseY);
 //square
 fill(mouseY, mouseX, 200);
 rect(x, 100, 100, 100);
 x += 3;
 if (x>width)(
     x=0
-)
+);
 
 }
