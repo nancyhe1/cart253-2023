441b649f5e0a5fa2bca94bd537f2011d9db817e2
nancyhe1 <nancyhe098@gmail.com>
2023-09-22 00:24:27 -0400

    finished watching 4.1 video

--- experiments/conditionals-experiment/js/script.js
+++ experiments/conditionals-experiment/js/script.js
@@ -8,25 +8,34 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
-
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 5
 }
 
-
-/**
- * Description of setup
-*/
 function setup() {
-
+  createCanvas(500,500);
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
+  background(backgroundShade);
 
+  circle.x = circle.x + circle.speed;
+
+if (circle.x > width) {
+    circle.speed = -circle.speed;
+}
+if (circle.x < 0) {
+    circle.speed = -circle.speed;
+}
+if (mouseY < height/2) {
+    fill(255,0,0); 
+}
+if (mouseY > height/2) {
+    fill(0,0,255);
+}
+  ellipse(circle.x,circle.y,circle.size);
 }
