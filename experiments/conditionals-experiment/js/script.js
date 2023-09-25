a64751cce5bfc46903c698df506d4ae9e75be339
nancyhe1 <nancyhe098@gmail.com>
2023-09-25 17:45:06 -0400

    finished watching video 4.4

--- experiments/conditionals-experiment/js/script.js
+++ experiments/conditionals-experiment/js/script.js
@@ -8,7 +8,11 @@
 
 "use strict";
 
-let displayCircle = false;
+let caterpillar = {
+  x:100,
+  y:250,
+  segmentSize:50
+}
 
 function setup() {
   createCanvas(500,500);
@@ -16,13 +20,24 @@ function setup() {
 
 function draw() {
   background(0);
+  noStroke();
+  fill(100,200,100);
+  
+  //let x=caterpillar.x;
+  //let numSegments = 10;
+  //let segmentsDrawn = 0
 
-  if (mouseIsPressed) {
-    displayCircle = true;
-  }
+ // while (segmentsDrawn < numSegments) {
+ //   ellipse(x,caterpillar.y, caterpillar.segmentSize);
+  //x = x + 40;
+  //segmentsDrawn++;
+  //}
 
-  if (displayCircle) {
-    ellipse(250,250,100,100);
-  }
+  let x = caterpillar.x;
+  let numSegments = 10;
+   for (let i = 0; i < numSegments; i++) {
+    ellipse(x,caterpillar.y,caterpillar.segmentSize);
+    x = x + 40; 
+   }
   
 }
