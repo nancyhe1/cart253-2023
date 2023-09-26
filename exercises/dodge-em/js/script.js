ea052c4fafb7f53904c4d3c0361b7bce6b7e5744
nancyhe1 <nancyhe098@gmail.com>
2023-09-26 15:58:07 -0400

    E2: added a more interesting movement for covid19 and add text in the game over display

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -3,6 +3,11 @@
  * Nancy He
  * 
  * this my attempt of making a covid19 dodge game.
+ * 
+ * images from:
+ * virus image from flaticon
+ * smiling emoji from emoji island
+ * crying emoji from emoji island
  */
 
 "use strict";
@@ -10,14 +15,17 @@
 //Variables
 let covid19 = {
     x: 0,
-    y: 250,
+    y: 0,
     size: 100,
     vx: 0,
-    vy: 3,
-    ax: 2,
-    ay: 1,
-    speed: 5,
-    image: undefined
+    vy: 0,
+    ax: 0,
+    ay: 0,
+    acceleration: 0.2,
+    speed: 0.25,
+    image: undefined,
+    xoff: 0.0,
+    yoff: 0.0
 };
 let user = {
     x: 250,
@@ -27,10 +35,11 @@ let user = {
     ax: 0,
     ay: 0,
     acceleration: 0.2,
-    maxSpeed: 6,
+    maxSpeed: 10,
     size: 100,
     fill: 255,
-    image: undefined
+    image: undefined,
+    dieImage: undefined
 }
 
 let numStatic = 1000;
@@ -38,6 +47,7 @@ let numStatic = 1000;
 function preload() {
     covid19.image = loadImage("assets/images/covid19 virus.png");
     user.image = loadImage("assets/images/smiling emoji.webp");
+    user.dieImage = loadImage("assets/images/crying emoji.webp");
 }
 
 /**
@@ -59,7 +69,7 @@ function setup() {
 */
 function draw() {
     background(0);
-
+    
     //display static
     for (let i = 0; i < numStatic; i++) {
         let x = random(0,width);
@@ -67,7 +77,7 @@ function draw() {
         stroke(255,255,255,90);
         strokeWeight(2);
         point(x,y);
-    
+        
     }
     for (let i = 0; i < numStatic; i++) {
         let x = random(0,width);
@@ -75,52 +85,65 @@ function draw() {
         stroke(255);
         strokeWeight(3);
         point(x,y);
-    
+        
     }
     
-    //covid movement
-    covid19.x = covid19.x + covid19.vx;
-    covid19.y = covid19.y + covid19.vy;
-
-    if (covid19.x > width) {
-        covid19.x = 0;
-        covid19.y = random(0,height);
-    } 
-
-
     //user movement
     if (mouseX < user.x) {
         user.ax = -user.acceleration;
-      }
-      else {
+    }
+    else {
         user.ax = user.acceleration;
-      }
-      if (mouseY < user.y) {
+    }
+    if (mouseY < user.y) {
         user.ay =-user.acceleration;
-      }
-      else {
+    }
+    else {
         user.ay = user.acceleration;
-      }
+    }
     user.vx = user.vx + user.ax;
     user.vx = constrain(user.vx,-user.maxSpeed,user.maxSpeed);
     user.vy = user.vy + user.ay;
     user.vy = constrain(user.vy,-user.maxSpeed,user.maxSpeed);
-
+    
     user.x = user.x + user.vx;
     user.y = user.y + user.vy;
+    
+    //covid movement
 
-    //check for catching covid19
-    let d = dist(user.x,user.y,covid19.x,covid19.y);
-    if (d < covid19.size/2 + user.size/2) {
-        noLoop();
-    }
+    covid19.x = random(0,width);
+    covid19.y = random(0,height);
+    //covid19.x = covid19.x + covid19.vx;
+    //covid19.y = covid19.y + covid19.vy;
+    
+    //if (covid19.y > height) {
+       // covid19.x = random(0,width);
+        //covid19.y = 0;
+    //}
 
+    covid19.xoff = covid19.xoff + 0.01;
+    covid19.x = noise(covid19.xoff) * width;
+    covid19.yoff = covid19.yoff + 0.03;
+    covid19.y = noise(covid19.yoff)* height;
+    
+    
     //Display covid19
     imageMode(CENTER)
     image(covid19.image,covid19.x,covid19.y,covid19.size,covid19.size);
 
     //display user
     image(user.image,user.x,user.y,user.size,user.size);
+    
+    //check for catching covid19
+    let d = dist(user.x,user.y,covid19.x,covid19.y);
+    if (d < covid19.size/2 + user.size/2) {
+        noLoop();
+        image(user.dieImage,user.x,user.y,user.size,user.size);
+        textAlign(CENTER);
+        textSize(40);
+        text('YOU CAUGHT COVID!', windowWidth/2, windowHeight/2);
+    }
+
 
 
 }
