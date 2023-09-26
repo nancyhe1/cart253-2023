a453ff75f00eaa2a00cbd03cb4730e0635cd6ed7
nancyhe1 <nancyhe098@gmail.com>
2023-09-26 00:39:31 -0400

    E2: started with the covid19 activity

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Title of Project
+ * Exercise 2: Dodge em
  * Nancy He
  * 
  * This is a template. You must fill in the title, author, 
@@ -8,19 +8,38 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
-
+let covid19 = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+    fill: {
+        r: 255,
+        g: 0,
+        b: 0
+    }
+};
+let user = {
+    x: 250,
+    y: 250,
+    size: 100,
+    fill: 255
 }
 
+let numStatic = 5000;
 
 /**
  * Description of setup
 */
 function setup() {
+    createCanvas(windowWidth,windowHeight);
 
+    covid19.y = random(0,height);
+    covid19.vx = covid19.speed;
+
+    noCursor();
 }
 
 
@@ -28,5 +47,43 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+
+    //display static
+    for (let i = 0; i < numStatic; i++) {
+        let x = random(0,width);
+        let y = random(0,height);
+        stroke(255);
+        point(x,y);
+    
+    }
+    
+    //covid movement
+    covid19.x = covid19.x + covid19.vx;
+    covid19.y = covid19.y + covid19.vy;
+
+    if (covid19.x > width) {
+        covid19.x = 0;
+        covid19.y = random(0,height);
+    } 
+
+    //user movement
+    user.x = mouseX;
+    user.y = mouseY;
+
+    //check for catching covid19
+    let d = dist(user.x,user.y,covid19.x,covid19.y);
+    if (d < covid19.size/2 + user.size/2) {
+        noLoop();
+    }
+
+    //Display covid19
+    fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+    ellipse(covid19.x,covid19.y,covid19.size);
+
+    //display user
+    fill(user.fill);
+    ellipse(user.x,user.y,user.size);
+
 
 }
