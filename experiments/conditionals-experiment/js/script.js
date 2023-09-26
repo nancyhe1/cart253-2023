325f108d90cd32989242f9aafbff65d30b365f6a
nancyhe1 <nancyhe098@gmail.com>
2023-09-25 21:58:51 -0400

    finished watching video 4.5

--- experiments/conditionals-experiment/js/script.js
+++ experiments/conditionals-experiment/js/script.js
@@ -8,36 +8,42 @@
 
 "use strict";
 
-let caterpillar = {
-  x:100,
+let circle = {
+  x: undefined,
+  y: undefined,
+  size:100
+}
+let dangerZone = {
+  x:250,
   y:250,
-  segmentSize:50
+  size:150
 }
 
 function setup() {
   createCanvas(500,500);
+
+  circle.x = random(0, width);
+  circle.y = random(0, height);
+
+  let d = dist(circle.x, circle.y, dangerZone.x, dangerZone.y);
+  while (d < circle.size/2 + dangerZone.size/2); {
+  circle.x = random(0,width);
+  circle.y = random(0,height);
+  d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
+  }
+
 }
 
 function draw() {
   background(0);
+  
+  //Danger zone
+  noFill();
+  stroke(255,0,0);
+  ellipse(dangerZone.x, dangerZone.y, dangerZone.size);
+  
+  //circle
+  fill(255);
   noStroke();
-  fill(100,200,100);
-  
-  //let x=caterpillar.x;
-  //let numSegments = 10;
-  //let segmentsDrawn = 0
-
- // while (segmentsDrawn < numSegments) {
- //   ellipse(x,caterpillar.y, caterpillar.segmentSize);
-  //x = x + 40;
-  //segmentsDrawn++;
-  //}
-
-  let x = caterpillar.x;
-  let numSegments = 10;
-   for (let i = 0; i < numSegments; i++) {
-    ellipse(x,caterpillar.y,caterpillar.segmentSize);
-    x = x + 40; 
-   }
-  
+  ellipse(circle.x, circle.y, circle.size);
 }
