e03a87e7257f85db356280f8a13a974baed57412
nancyhe1 <nancyhe098@gmail.com>
2023-09-26 00:30:46 -0400

    finished watching video 4.11

--- experiments/conditionals-experiment/js/script.js
+++ experiments/conditionals-experiment/js/script.js
@@ -7,21 +7,36 @@
  */
 
 "use strict";
-let clownImage;
+let clown = {
+  x: 250,
+  y: 250,
+  size: 100,
+  image: undefined // Not loaded yet
+};
 
 function preload() {
-  clownImage = loadImage("assets/images/slown.png");
+  // Load the clown image
+  clown.image = loadImage("assets/images/clown.png");
 }
 
 function setup() {
   createCanvas(500,500);
-
 }
 
 function draw() {
   background(0);
 
+  // Position the clown at the mouse position
+  clown.x = mouseX;
+  clown.y = mouseY;
+
+  // Display the clown (we're using the size arguments so we can change size)
   imageMode(CENTER);
-  image(clownImage,mouseX,mouseY)
+  image(clown.image,clown.x,clown.y,clown.size,clown.size);
+}
+
+function mousePressed() {
+  // Increase the clown's size when the user clicks
+  clown.size = clown.size + 50;
 }
 
