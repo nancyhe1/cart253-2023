de48a1a441777a94a411edb5a9ea9708e32d0959
nancyhe1 <nancyhe098@gmail.com>
2023-10-02 15:46:29 -0400

    started function experiment

---
+++ experiments/functions-experiment/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Title of Project
+ * Nancy He
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+createCanvas(500,500);
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+background(0);
+ellipse(width/2,height/2,100,100);
+}
