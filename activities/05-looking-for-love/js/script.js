69a17da4389b14edb2a984cdb4df73c98d1bf82c
nancyhe1 <nancyhe098@gmail.com>
2023-10-03 02:05:23 -0400

    A5: finished activity!

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
+ * Activity 5: Looking for love
  * Nancy He
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * simple love game with 2 circles
  */
 
 "use strict";
@@ -25,6 +24,8 @@ let circle2 = {
     vy: 0,
     speed:5,
 };
+
+let state = 'title'; // can be: title, simulation, love , sadness
 
 /**
  * Description of preload
@@ -59,18 +60,96 @@ circle2.vy = random(-circle2.speed,circle2.speed);
 */
 function draw() {
 background(0);
-//move the circles
+
+if (state === 'title') {
+title();
+}
+else if (state === 'simulation') {
+    simulation();
+
+}
+else if (state === 'love') {
+love();
+}
+else if (state === 'sadness') {
+sadness();
+}
+}
+
+function title() {
+    push();
+    textSize(64)
+    fill(200,100,100);
+    textAlign(CENTER,CENTER);
+    text('LOVE?', width/2, height/2);
+    pop();
+}
+
+function simulation() {
+move();
+checkOffScreen();
+checkOverlap();
+display();
+
+}
+
+function love() {
+    push();
+    textSize(64)
+    fill(200,100,100);
+    textAlign(CENTER,CENTER);
+    text('LOVE!', width/2, height/2);
+    pop();
+}
+
+function sadness() {
+    push();
+    textSize(64)
+    fill(127);
+    textAlign(CENTER,CENTER);
+    text(';((', width/2, height/2);
+    pop(); 
+}
+
+function move() {
+    //move the circles
 circle1.x = circle1.x + circle1.vx;
 circle1.y = circle1.y + circle1.vy;
 
 circle2.x = circle2.x + circle2.vx;
 circle2.y = circle2.y + circle2.vy;
+}
 
-//check if the circles have gone offscreen
-if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1 > height ||circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2 > height );
+function checkOffScreen() {
+    //check if the circles have gone offscreen
+    if (isOffScreen(circle1) || isOffScreen(circle2)) {
+    state = 'sadness';
+};
+}
+function isOffScreen(circle) {
+    if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function checkOverlap() {
+    //check if the circles overlap
+let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
+if (d < circle1.size/2 + circle2.size/2) {
+    state = 'love';
+}
+}
 
+function display() {
 //display the circles
 ellipse(circle1.x, circle1.y, circle1.size);
 ellipse(circle2.x, circle2.y, circle2.size);
+}
 
+function mousePressed() {
+    if (state === 'title') {
+        state = 'simulation';
+    }
 }
