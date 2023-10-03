d9d9f601e9bce784a34bd65b70406650584775d4
nancyhe1 <nancyhe098@gmail.com>
2023-10-02 22:59:38 -0400

    finished watching 5.3

--- experiments/functions-experiment/js/script.js
+++ experiments/functions-experiment/js/script.js
@@ -8,24 +8,45 @@
 
 "use strict";
 
-function setup() {
-    createCanvas(500,500);
-
-    let hotCelsius = toCelsius(100);
-    console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`);
-
-    let coldCelsius = toCelsius(10);
-    console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`);
-
-
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0
+  }
+  
+  function setup() {
+    createCanvas(500, 500)
+    reset();
   }
   
   function draw() {
     background(0);
   
+    move();
+  
+    let offScreen = circleIsOffScreen();
+    if (offScreen) {
+      reset();
+    }
+  
+    ellipse(circle.x, circle.y, circle.size);
   }
-
-  function toCelsius(fahrenheit) {
-    let celsius = (fahrenheit-32)*5/9;
-    return celsius;
+  
+  function circleIsOffScreen() {
+    let result = (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height);
+    return result;
+  }
+  
+  function move() {
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+  }
+  
+  function reset() {
+    circle.x = 250;
+    circle.y = 250;
+    circle.vx = random(-10, 10);
+    circle.vy = random(-10, 10);
   }
