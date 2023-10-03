d7c1b6c53a7456cdf9d24c0dc8ef21f75bc50f19
nancyhe1 <nancyhe098@gmail.com>
2023-10-03 01:38:13 -0400

    A5: made the circles move

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -9,8 +9,8 @@
 "use strict";
 
 let circle1 = {
-    x: width/3,
-    y: height/2,
+    x: 0,
+    y: 0,
     size: 100,
     vx: 0,
     vy: 0,
@@ -18,8 +18,8 @@ let circle1 = {
 };
 
 let circle2 = {
-    x: width*2/3,
-    y: height/2,
+    x: 0,
+    y: 0,
     size: 100,
     vx: 0,
     vy: 0,
@@ -39,6 +39,18 @@ function preload() {
 */
 function setup() {
 createCanvas(windowWidth,windowHeight);
+
+circle1.x = width/3;
+circle1.y = height/2;
+
+circle2.x = width*2/3;
+circle2.y = height/2;
+
+//start circles moving in random direction
+circle1.vx = random(-circle1.speed,circle1.speed);
+circle2.vx = random(-circle2.speed,circle2.speed);
+circle1.vy = random(-circle1.speed,circle1.speed);
+circle2.vy = random(-circle2.speed,circle2.speed);
 }
 
 
@@ -46,5 +58,19 @@ createCanvas(windowWidth,windowHeight);
  * Description of draw()
 */
 function draw() {
+background(0);
+//move the circles
+circle1.x = circle1.x + circle1.vx;
+circle1.y = circle1.y + circle1.vy;
+
+circle2.x = circle2.x + circle2.vx;
+circle2.y = circle2.y + circle2.vy;
+
+//check if the circles have gone offscreen
+if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1 > height ||circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2 > height );
+
+//display the circles
+ellipse(circle1.x, circle1.y, circle1.size);
+ellipse(circle2.x, circle2.y, circle2.size);
 
 }
