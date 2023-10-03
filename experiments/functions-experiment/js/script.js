122efab05fcaa5aea9086d78735635b1784a677d
nancyhe1 <nancyhe098@gmail.com>
2023-10-02 22:09:22 -0400

    using return for celsius and fahrenheit

--- experiments/functions-experiment/js/script.js
+++ experiments/functions-experiment/js/script.js
@@ -9,25 +9,23 @@
 "use strict";
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(500,500);
+
+    let hotCelsius = toCelsius(100);
+    console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`);
+
+    let coldCelsius = toCelsius(10);
+    console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`);
+
+
   }
   
   function draw() {
     background(0);
-
-    parallels(100,100,5,3,100,10);
-    parallels(50,50,10,1,20,5);
-    parallels(200,200,15,7,3,20);
-    parallels(312,257,20,0.5,300,2);
   
-   function parallels(x,y,numLines, lineWidth,lineHeight,lineSpacing) {
-     
-     for (let i = 0; i < numLines; i++) {
-       noStroke();
-       fill(255);
-       rectMode(CENTER);
-       rect(x, y, lineWidth, lineHeight);
-       x = x + lineSpacing;
-     }
-   }
+  }
+
+  function toCelsius(fahrenheit) {
+    let celsius = (fahrenheit-32)*5/9;
+    return celsius;
   }
