5344005291b43c09d626b77f1b53767c569ada13
nancyhe1 <nancyhe098@gmail.com>
2023-10-03 00:12:00 -0400

    finished video 5.5

--- experiments/functions-experiment/js/script.js
+++ experiments/functions-experiment/js/script.js
@@ -7,33 +7,66 @@
  */
 
 "use strict";
-let hello = {
-    string: `Hello, World!`,
-    x:0,
-    y:0,
-    vx:5,
-    vy:1,
-    size:64,
-};
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 2,
+  };
 
-function setup() {
-    createCanvas(500,500);
+  let state = 'title'; //possible states are: title, animation, ending
+  
+  function setup() {
+    createCanvas(500, 500);
+    circle.vx = circle.speed;
+
+    textSize(32);
+    textAlign(CENTER,CENTER);
   }
   
   function draw() {
     background(0);
 
-    hello.x = hello.x+hello.vx;
-    hello.y = hello.y+hello.vy;
-    hello.size = hello.size + 1;
+    if(state === 'title') {
+        title();
+    }
+    else if(state==='animation') {
+        animation();
+    }
+    else if(state==='ending') {
+        ending();
+    }
+  }
 
-    textAlign(CENTER,CENTER);
-    textSize(hello.size);
-    textStyle(BOLD);
+    function title() {
+    //title
+    fill(255);
+    text('Life.',width/2,height/2);
+    }
 
-    stroke(50,200,50);
-    strokeWeight(3);
+    function animation() {
+    //animation
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
 
-    fill(200,50,200);
-    text(hello.string,hello.x,hello.y);
+    if(circle.x>width) {
+        state = 'ending';
+    }
+    
+    ellipse(circle.x, circle.y, circle.size);
+    }
+
+   function ending() {
+    //ending
+    fill(127);
+    text('its all over.',width/2,height/2);
+    }
+
+
+  function keyPressed() {
+    if(state === 'title') {
+    state = 'animation';
+    }
   }
