8bbc7afd9cf8c11ebb32493093d2da3720ac58f3
nancyhe1 <nancyhe098@gmail.com>
2023-10-11 17:08:40 -0400

    P1: added a title screen and a key press function

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -21,6 +21,7 @@ let canvas = {
     w:700,
     h:500,
 }
+let state = 'title'; //can be: title...
 
 /**
  * loading images
@@ -44,14 +45,26 @@ textAlign(CENTER);
  * all the game animation
 */
 function draw() {
-    background(198, 232, 169); //grass
+   
     playerControls();
     
-display();
+
+if (state === 'title') {
+  title();
+  }
+  else if (state === 'simulation') {
+    simulation();
+
+}
+}
+function simulation() {
+  background(198, 232, 169); //grass
+  display(); 
 }
 //player controls
 function playerControls() {
     if (keyIsDown(65)) { //key code for 'A'
+      console.log("A is pressed");
         player.vx = -player.speed;
       }
       else if (keyIsDown(68)) { //keycode for 'D' 
@@ -73,7 +86,17 @@ function playerControls() {
 player.x = player.x + player.vx;
 player.y = player.y + player.vy; 
 }
-
+function title() {
+  push();
+  background(152, 213, 237);
+  textSize(30);
+  fill(255);
+  text('Welcome to this calming Gardening Simulator!', width/2,height/2);
+  textSize(20);
+  text("Use 'AWSD' keys to move around",width/2,height/2+20);
+  text('Press any key to Start', width/2,height/2+40);
+  pop();
+}
 
 function display() {
 //player appearance
@@ -81,5 +104,12 @@ noStroke();
 fill(181, 115, 209);
 ellipse(player.x,player.y,player.size);
 
+}
+
+function keyPressed() {
+  if (state === 'title') {
+      state = 'simulation';
+  }
+  
 }
 
