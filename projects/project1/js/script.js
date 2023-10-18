c606414f5ff16dbd3919c37e9c8b5031d3ea6cc3
nancyhe1 <nancyhe098@gmail.com>
2023-10-17 23:53:44 -0400

    P1: made overlapping functions

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -53,11 +53,18 @@ let waterCan = {
 let plant = {
   x:undefined,
   y:undefined,
-  size:200,
+  size:400,
   image: undefined,
-  sprout: undefined,
   active: true 
 }
+let smallSprout = {
+  x:undefined,
+  y:undefined,
+  size:400,
+  image: undefined,
+  active: true 
+}
+
 let shovel = {
   x:undefined,
   y:undefined,
@@ -72,6 +79,7 @@ let dirt = {
   image:undefined,
   active: true,
 }
+let time = 1;
 let state = 'title'; //can be: title...
 //let customFont;
 
@@ -84,7 +92,8 @@ player.texture2 = loadImage("assets/images/closed-glove.png");
 seedBag.image = loadImage("assets/images/seed-bag.png");
 waterCan.image = loadImage("assets/images/can.png");
 waterCan.pour = loadImage("assets/images/water.png");
-//plant.sprout = loadImage("assets/images/sprout.png");
+plant.image = loadImage("assets/images/potwithseed.png");
+smallSprout.image = loadImage("assets/images/lilsprout.png");
 shovel.image = loadImage("assets/images/shovel.png");
 dirt.image = loadImage("assets/images/potwithdirt.png")
 pot.image = loadImage("assets/images/pot.png");
@@ -117,8 +126,8 @@ function draw() {
   }
 }
 function toolSetUp() {
-  pot.x = width/2;
-  pot.y = height/2+200;
+  pot.x = 500;
+  pot.y = 600;
   seedBag.x= 150;
   seedBag.y= 100;
   waterCan.x = 150;
@@ -132,12 +141,15 @@ function simulation() {
   background(198, 232, 169); //grass
   instruction();
   displayImage(); 
+  CheckOverlapShovel();
+  CheckOverlapSeedBag();
+  watering();
 } 
 
 function instruction() {
   instructionBox.x = width*8/9;
   instructionBox.y = height/2; 
-  let controlText ="-First put some dirt in the pot with the help of the shovel -To plant a seed, drag the seed to the dirt and press 'P' -To water the plant, hover over the plant and press 'W', it will grow as you water it"
+  let controlText ="-First, put some dirt in the pot, drag the shovel to the pot -Second, to plant a seed, drag the seed bag to the dirt -To water the plant, drag the watering can to the pot and press 'W', it will grow as you water it"
   fill(255);
   rect(instructionBox.x,instructionBox.y,instructionBox.w,instructionBox.h);
   textSize(20);
@@ -145,16 +157,16 @@ function instruction() {
   text(controlText,instructionBox.x,instructionBox.y+50,instructionBox.w,instructionBox.h);
 }
 
+
+//player controls
+function playerControls() {
+  player.x = mouseX;
+  player.y = mouseY;
+  player.x = player.x + player.vx;
+  player.y = player.y + player.vy; 
   
-  //player controls
-  function playerControls() {
-    player.x = mouseX;
-    player.y = mouseY;
-    player.x = player.x + player.vx;
-    player.y = player.y + player.vy; 
-    
-    //constrain the player withing the canvas
-    player.x = constrain(player.x,0,width);
+  //constrain the player withing the canvas
+  player.x = constrain(player.x,0,width);
     player.y = constrain(player.y,0,height);
   }
   
@@ -172,17 +184,21 @@ function instruction() {
   
   function displayImage() {
     //drawing the pot
-  image(pot.image,pot.x,pot.y,pot.size,pot.size);
-  
-  //drawing the seed of bag
-  image(seedBag.image,seedBag.x,seedBag.y,seedBag.size,seedBag.size);
-  
-  // drawing the shovel
-  image(shovel.image,shovel.x,shovel.y,shovel.size,shovel.size);
-  
-  //watercan
-  if (keyIsDown(87)) { //use 'W' key
-    image(waterCan.pour,waterCan.x,waterCan.y,waterCan.size,waterCan.size);
+    image(pot.image,pot.x,pot.y,pot.size,pot.size);
+    
+    image(dirt.image, dirt.x, dirt.y, dirt.size, dirt.size);
+    image(plant.image, plant.x, plant.y, plant.size, plant.size);
+    image(smallSprout.image,smallSprout.x,smallSprout.y,smallSprout.size,smallSprout.size);
+    
+    //drawing the seed of bag
+    image(seedBag.image,seedBag.x,seedBag.y,seedBag.size,seedBag.size);
+    
+    // drawing the shovel
+    image(shovel.image,shovel.x,shovel.y,shovel.size,shovel.size);
+    
+    //watercan
+    if (keyIsDown(87)) { //use 'W' key
+      image(waterCan.pour,waterCan.x,waterCan.y,waterCan.size,waterCan.size);
   }
   else {
     image(waterCan.image,waterCan.x,waterCan.y,waterCan.size,waterCan.size);
@@ -200,30 +216,47 @@ function instruction() {
 }
 
 function shoveling() {
-  //drawing the pot with dirt
-  image(dirt.image, dirt.x, dirt.y, dirt.size, dirt.size);
-  dirt.x = width/2;
-  dirt.y = height/2+200; 
+  dirt.x = 500;
+  dirt.y = 600; 
+}
+
+function planting(){
+  plant.x=500;
+  plant.y=600;
 }
 
 function keyPressed() {
   if (state === 'title') {
     state = 'simulation';
-    if(keyCode === 83) {
-      state = 'growth1';
-    }
   } 
+  
 }
 
-function DraggingSeedBad() {
-      if (seedBag.isBeingDragged) {
-          seedBag.x = mouseX + seedBag.offSetX;
-          seedBag.y = mouseY + seedBag.offSetY;
-  
-          seedBag.x = constrain(seedBag.x, 0, width);
-          seedBag.y = constrain(seedBag.y, 0, height);
-      }
+function CheckOverlapShovel() {
+  let d = dist(shovel.x, shovel.y, pot.x, pot.y);
+  if (d < shovel.size/3 + pot.size/3) {
+      shoveling();
   }
+}
+
+function CheckOverlapSeedBag() {
+  let d = dist(seedBag.x, seedBag.y, pot.x, pot.y);
+  if (d < seedBag.size/3 + pot.size/3) {
+    planting();
+      
+  }
+}
+
+function watering() {
+  if (keyIsDown(87)) {
+    time += 1;
+    if (time = 5) {
+      smallSprout.x = 500;
+      smallSprout.y = 600;
+    }
+  }
+
+}
 function mouseDragged() {
   if ((mouseX > waterCan.x - 100) && (mouseX < waterCan.x + 100)) {
     if ((mouseY > waterCan.y - 100) && (mouseY < waterCan.y + 100)) {
