c20788f0ae932833323b839da86f47eb099245be
nancyhe1 <nancyhe098@gmail.com>
2023-10-30 18:01:46 -0400

    A7: set up the balls

--- activities/07-object-oriented-programming-activity/js/script.js
+++ activities/07-object-oriented-programming-activity/js/script.js
@@ -7,6 +7,12 @@
  */
 
 "use strict";
+let gravityForce = 0.0025;
+
+let paddle;
+
+let balls = [];
+let numBalls = 3;
 
 /**
  * Description of preload
@@ -20,6 +26,16 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(windowWidth,windowHeight);
+
+    paddle = new Paddle(300,20);
+
+    for (let i = 0; i <numBalls; i++) {
+        let x =  random(0,width);
+        let y = random(-400,-100);
+        let ball = new Ball(x,y);
+        balls.push(ball);
+    }
 
 }
 
@@ -28,5 +44,16 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    paddle.move();
+    paddle.display();
+
+    for(let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+        ball.gravity(gravityForce);
+        ball.move();
+        ball.bounce();
+        ball.display();
+    }
 }
