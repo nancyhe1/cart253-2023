38ca3a0f94c0516981a7f7801b6ee905761b6dce
nancyhe1 <nancyhe098@gmail.com>
2023-11-13 17:08:38 -0500

    finished watching video 10.1

--- experiments/sound-experiment/js/script.js
+++ experiments/sound-experiment/js/script.js
@@ -8,21 +8,46 @@
 
 "use strict";
 
-let circle = {
-    x: 0,
-    y: 0,
-    size: 100
+let barkSFX;
+// Whether to display "BARK!"
+let showBarkText = false;
+
+function preload() {
+  barkSFX = loadSound(`assets/sounds/bark.wav`);
+}
+
+function setup() {
+  createCanvas(600, 600);
+  userStartAudio();
+
+  // Add cues to our sound at specific times (in seconds)
+  barkSFX.addCue(0.1, showBark);
+  barkSFX.addCue(0.3, hideBark);
+  barkSFX.addCue(0.4, showBark);
+  barkSFX.addCue(0.7, hideBark);
+}
+
+function draw() {
+  background(0);
+
+  if (showBarkText) {
+    push();
+    fill(255);
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    text(`BARK!`, width / 2, height / 2);
+    pop();
   }
-  
-  function setup() {
-    createCanvas(500, 500);
-  }
-  
-  function draw() {
-    background(0);
-  
-    circle.x = mouseX;
-    circle.y = mouseY; 
-  
-    ellipse(circle.x, circle.y, 100);
-  }
+}
+
+function showBark() {
+  showBarkText = true;
+}
+
+function hideBark() {
+  showBarkText = false;
+}
+
+function mousePressed() {
+  barkSFX.play();
+}
