d6903953d5f606c37e7117c7006cee4f0e84a9a9
nancyhe1 <nancyhe098@gmail.com>
2023-11-13 18:16:39 -0500

    watched video 10.4

--- experiments/sound-experiment/js/script.js
+++ experiments/sound-experiment/js/script.js
@@ -8,39 +8,65 @@
 
 "use strict";
 
-let synth;
-let notes = [`F4`, `G4`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F5`];
-let currentNote = 0;
-let interval;
+let mic;
+let ghost;
+let clownImage;
+
+function preload() {
+  clownImage = loadImage(`assets/images/clown.png`);
+}
 
 function setup() {
   createCanvas(600, 600);
-  userStartAudio();
 
-  synth = new p5.PolySynth();
+  mic = new p5.AudioIn();
+  mic.start();
+  ghost = createGhost(clownImage);
+}
+
+// ghostly properties
+function createGhost(ghostImage) {
+  let ghost = {
+    x: width / 2,
+    y: height / 2,
+    vx: 0,
+    vy: 0,
+    happySpeed: 1, 
+    scaredSpeed: 25, 
+    image: ghostImage, 
+    alpha: 50, 
+    state: `happy`, 
+    scaredThreshold: 0.5 
+  };
+  return ghost;
 }
 
 function draw() {
   background(0);
-}
 
-// mousePressed() starts and stops our piano playing
-function mousePressed() {
-  if (interval === undefined) {
-    interval = setInterval(playNextNote, 500);
-  }
-  else {
-    clearInterval(interval);
-    interval = undefined;
-  }
-}
+  let level = mic.getLevel();
 
-// playNextNote() plays the next note in our array
-function playNextNote() {
-  let note = notes[currentNote];
-  synth.play(note, 0.2, 0, 0.4);
-  currentNote = currentNote + 1;
-  if (currentNote === notes.length) {
-    currentNote = 0;
+  if (level > ghost.scaredThreshold) {
+    ghost.state = `afraid`;
+    ghost.vx = ghost.scaredSpeed;
   }
+
+  // Check if the ghost is happy at the moment
+  if (ghost.state === `happy`) {
+    let r = random(0, 1);
+    if (r < 0.1) {
+      ghost.vx = random(-ghost.happySpeed, ghost.happySpeed);
+      ghost.vy = random(-ghost.happySpeed, ghost.happySpeed);
+    }
+  }
+
+  // Move the ghost
+  ghost.x += ghost.vx;
+  ghost.y += ghost.vy;
+
+  // Display the ghost
+  push();
+  tint(255, ghost.alpha);
+  image(ghost.image, ghost.x, ghost.y);
+  pop();
 }
